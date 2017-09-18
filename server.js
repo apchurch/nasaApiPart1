9d60e88c8bd37065c2d14908e57aa0945157616f
Andrew Church <andrewchurch@Andrews-MacBook-Pro.local>
2017-09-17 23:58:10 -0600

    Add existing file

--- server.js
+++ server.js
@@ -1,38 +1,40 @@
-// var myRequest = new XMLHttpRequest()
-// 	myRequest.open('GET', 'https://api.nasa.gov/neo/rest/v1/feed?start_date=2017-07-23&end_date=2017-07-29&api_key=Lhfmo5GJFKkymB61crhYsjHIUjNc8Eq8iuFBr4pn')
-// 	myRequest.onload = function() {
-// 		var myData = JSON.parse(myRequest.responseText)
-		// console.log(myData)
-// var express = require('express')
-// var request = require('request')
-// var app = express()
+var express = require('express')
+var request = require('request')
+var app = express()
 
-// app.get('https://api.nasa.gov/neo/rest/v1/feed?start_date=2017-07-23&end_date=2017-07-29&api_key=Lhfmo5GJFKkymB61crhYsjHIUjNc8Eq8iuFBr4pn', function(req, res){
-// 	res.send()
-// })
+var myRequest = new request()
+	myRequest.open('GET', 'https://api.nasa.gov/neo/rest/v1/feed?start_date=2017-07-23&end_date=2017-07-29&api_key=Lhfmo5GJFKkymB61crhYsjHIUjNc8Eq8iuFBr4pn')
+	myRequest.onload = function() {
+		var myData = JSON.parse(myRequest.responseText)
+		console.log(myData)
+
+
+app.get('https://api.nasa.gov/neo/rest/v1/feed?start_date=2017-07-23&end_date=2017-07-29&api_key=Lhfmo5GJFKkymB61crhYsjHIUjNc8Eq8iuFBr4pn', function(req, res){
+	res.send()
+})
 		
-// 		app.post('/', function(req, res){
-// 	res.send(myData)
-// $("#asteroid-info").append(myData)
+		app.post('/', function(req, res){
+	res.send(myData)
+$("#asteroid-info").append(myData)
 	
-// })	
-// }	
-// myRequest.send()
+})	
+}	
+myRequest.send()
 
 
 
 
-// window.onload = function(){
-// 	var asteroidContainer = document.getElementById("asteroid-info")
-// 	function renderHTML(data) {
-// 	var JSON_obj = JSON.parse
-// // 	for(i = 0; i < data.length; i++){
-// // 		JSON_obj += "<p>" + data[i] + ".</p>"
+window.onload = function(){
+	var asteroidContainer = document.getElementById("asteroid-info")
+	function renderHTML(data) {
+	var JSON_obj = JSON.parse
+// 	for(i = 0; i < data.length; i++){
+// 		JSON_obj += "<p>" + data[i] + ".</p>"
 
 
-// // }
-
-
-// asteroidContainer.insertAdjacentHTML('beforeend', data)
 // }
-// }
+
+
+asteroidContainer.insertAdjacentHTML('beforeend', data)
+}
+}
